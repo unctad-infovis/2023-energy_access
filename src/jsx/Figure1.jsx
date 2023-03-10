2bae7ddf19cc522c873dc6ea9b11baa5efc0207f
Teemo Tebest <teemo.tebest@gmail.com>
2023-03-10 15:00:09 +0100

    Initial commit

---
+++ src/jsx/Figure1.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+
+// Load helpers.
+import CSVtoJSON from './helpers/CSVtoJSON.js';
+import ChartLine from './components/ChartLine.jsx';
+
+import '../styles/styles.less';
+
+function Figure1() {
+  // Data states.
+  const [dataFigure, setDataFigure] = useState(false);
+
+  const cleanData = (data) => data.map((el) => {
+    const labels = Object.keys(el).filter(val => val !== 'Name').map(val => Date.UTC(parseInt(val, 10), 0, 1));
+    const values = Object.values(el).map(val => (parseFloat(val))).filter(val => !Number.isNaN(val));
+
+    return ({
+      data: values.map((e, j) => ({
+        x: labels[j],
+        y: e
+      })),
+      name: el.Name
+    });
+  });
+
+  useEffect(() => {
+    const data_file = `${(window.location.href.includes('unctad.org')) ? 'https://storage.unctad.org/2023-energy_access/' : './'}assets/data/2023-energy_access_figure1.csv`;
+    try {
+      fetch(data_file)
+        .then((response) => {
+          if (!response.ok) {
+            throw Error(response.statusText);
+          }
+          return response.text();
+        })
+        .then(body => setDataFigure(cleanData(CSVtoJSON(body))));
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  return (
+    <div className="app">
+      {dataFigure && (
+      <ChartLine
+        idx="1"
+        data={dataFigure}
+        data_decimals={0}
+        note=""
+        source="UNCTAD secretariat calculations based on data from the World Bank"
+        subtitle="Share of the population with access to clean cooking fuels, Sub-Saharan Africa and worldwide, 2000 to 2020"
+        suffix=""
+        title="Most of Sub-Sharan population still lack the access to clean cooking fuels"
+        ylabel=""
+      />
+      )}
+      <noscript>Your browser does not support JavaScript!</noscript>
+    </div>
+  );
+}
+
+export default Figure1;
